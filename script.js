5fa265c303b6ae45b20ae6c846748ca62d98f38c
Ritter1111 <86166867+Ritter1111@users.noreply.github.com>
2022-07-19 15:55:29 +0300

    added about page content

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-var colorArray = ["#FFEFD5", "#90EE90", "#E6E6FA", "#BA55D3", "#87CEFA", "#808080", "#FFFFFF", "#FFA07A"];
-var i = 0;
+// var colorArray = ["#FFEFD5", "#90EE90", "#E6E6FA", "#BA55D3", "#87CEFA", "#808080", "#FFFFFF", "#FFA07A"];
+// var i = 0;
 
-function changeColor(){
-    document.body.style.background = colorArray[i];
-    i++;
-    if( i >= colorArray.length) {
-        i = 0;
-    }
-}
+// function changeColor(){
+//     document.body.style.background = colorArray[i];
+//     i++;
+//     if( i >= colorArray.length) {
+//         i = 0;
+//     }
+// }
